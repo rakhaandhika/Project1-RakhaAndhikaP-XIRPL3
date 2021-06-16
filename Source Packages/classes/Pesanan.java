2ab5eb2fd064ccbcfe5ef89fa2077c2741e6e7c7
rapvxx <rakha.andhika2407@gmail.com>
2021-06-16 22:13:00 +0700

    Modul D: Mencetak Struk

--- Source Packages/classes/Pesanan.java
+++ Source Packages/classes/Pesanan.java
@@ -6,7 +6,18 @@ public class Pesanan {
     private int jumlah;
     private String keterangan;
 
-    public Pesanan(Menu menu, int jumlah) { }
+    public Pesanan(Menu menu, int jumlah) {
+        this.menu = menu;
+        this.jumlah = jumlah;
+    }
+
+    public Menu getMenu() {
+        return menu;
+    }
+
+    public int getJumlah() {
+        return jumlah;
+    }
 
     public void setKeterangan(String keterangan) { }
 
